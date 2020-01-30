f6f5bd3ff3d50abe755e5712428cd6d155c5bb65
Samod Shetty <samoshet@cisco.com>
2020-01-29 23:41:21 -0800

    Cosmetic appearance changes

--- cipherSub.py
+++ cipherSub.py
@@ -8,11 +8,11 @@ if len(sys.argv) < 2:
 else:
     ciphertext = ' '.join(sys.argv[1:])
 
+letterFreqSorted=['E','T','A','O','I','N','S','H','R','D','L','C','U','M','W','F','G','Y','P','B','V','K','J','X','Q','Z']
 
-# Count of Characters in cipher text
+# Initialize the Count of Characters in cipher text
 charCount = {'A':0,'B':0,'C':0,'D':0,'E':0,'F':0,'G':0,'H':0,'I':0,'J':0,'K':0,'L':0,'M':0,'N':0,'P':0,'Q':0,'R':0,'S':0,'T':0,'U':0,'V':0,'W':0,'X':0,'Y':0,'Z':0} 
 
- 
 for keys in ciphertext: 
     charCount[keys] = charCount.get(keys,0) + 1
 
@@ -22,9 +22,6 @@ print("\n\nLetter Occurences in Cipher Text:"+str(charCount))
 sortedList = sorted(charCount , key=charCount.__getitem__, reverse=True)
 
 print("\n\nSorted Letter Occurences in Cipher Text:"+str(sortedList))
-
-letterFreqSorted=['E','T','A','O','I','N','S','H','R','D','L','C','U','M','W','F','G','Y','P','B','V','K','J','X','Q','Z']
-
 
 # from sorted frequency Array and Sorted occurence array get the possible cipher key array 
 mapArr = {}
@@ -37,7 +34,7 @@ for keys in sortedList:
 # sort the array to get the cipher key
 mapArrSorted= sorted(mapArr, key=mapArr.__getitem__); 
 
-print("\n\n Mapping Array:"+str(mapArrSorted)); 
+print("\n\nMapping Array:"+str(mapArrSorted)); 
 
 
 # get cipher Key text
